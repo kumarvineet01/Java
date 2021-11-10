eb40f9e5a5abc062377ea6706686a2ba8886cd75
Swati <83855603+SwatiAF@users.noreply.github.com>
2021-11-10 16:37:21 +0530

    Lab Program 1

--- quadratic.java
+++ quadratic.java
@@ -6,19 +6,19 @@ double r1, r2;
 
 if( det > 0)
 {
-r1= (-b + Math.sqrt(det)) / (2*a);
-r2 = (-b - Math.sqrt(det)) / (2*a);
+  r1= (-b + Math.sqrt(det)) / (2*a);
+  r2 = (-b - Math.sqrt(det)) / (2*a);
 System.out.println("r1: " + r1 + "r2: " + r2);
 }
 else if(det==0)
 {
-r1=r2= -b/(2*a);
-System.out.println("r1=r2= "+r1);
+  r1=r2= -b/(2*a);
+  System.out.println("r1=r2= "+r1);
 }
 else
 {
-double real= -b/(2*a);
-double img= Math.sqrt(-det)/(2*a);
+  double real= -b/(2*a);
+  double img= Math.sqrt(-det)/(2*a);
 System.out.println("r1= " + real - imgi);
 System.out.println("r2= " + real + "+" imgi);
 }

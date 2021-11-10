f622a0b8df1c1f8b94801cac2777cdd2c4c79e15
Swati <83855603+SwatiAF@users.noreply.github.com>
2021-11-10 12:56:36 +0530

    SGPA included

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 class Details {
-	
+
  String name;
  String usn;
  int[] credits= new int[10];
@@ -10,21 +10,22 @@ class Details {
  int i, n;
 
 
+
 void input()
 {	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the Name of the Student: ");
+	System.out.print("Enter the Name of the Student: ");
 	name = sc.nextLine();
-	System.out.println("Enter the USN of the Student: ");
+	System.out.print("Enter the USN of the Student: ");
 	usn = sc.nextLine();
-	System.out.println("Enter number of Subjects: ");
+	System.out.print("Enter number of Subjects: ");
 	n = sc.nextInt();
 	//Inserting credits in the Array 
-	System.out.println("Enter the Credits of the Student: ");
+	System.out.print("Enter the Credits of the Student: ");
 	for(i=0; i<n; i++) {
 		credits[i] = sc.nextInt(); 
 	}
 	//Inserting Marks in the Array
-	System.out.println("Enter the Marks obtained in Subjects: ");
+	System.out.print("Enter the Marks obtained in Subjects: ");
 	for(i=0; i<n; i++) {
 		marks[i] = sc.nextInt();
 	}
@@ -35,29 +36,34 @@ void input()
 	int marks[];
 	int[] gradePoint = marks[i] / 10;
 	float[] Sgpa = (gradePoint[i]*credits[i]) / totalCredit;
-	total
+	for(i=0; i<n; i++)
+	{
+		totalCredit += credits[i];
+		i++; 
+	}
+}
 */
-
 void display()
 {
 	System.out.println("Displaying the Student Details: ");
-	System.out.println("NAME: " + name);
-	System.out.println("USN: " + usn);
+	System.out.println("NAME:  " + name);
+	System.out.println("USN:   " + usn);
 	for(i=0; i<n; i++)
 	{
-		System.out.println("CREDITS: " + credits[i]);
-		System.out.println("MARKS: " + marks[i]);
+		System.out.println("CREDITS:  " + credits[i]);
+		System.out.println("MARKS:  " + marks[i]);
 	}
 }
 }
+
 class Student {
 public static void main(String[] args) {
 
 	Details stud1 = new Details();
 	stud1.input();
 	stud1.display();
+	/*stud1.sgpa(); */
 }
 }	
-
 
 

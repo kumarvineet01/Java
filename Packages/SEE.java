fde3a64b483c406b08b79bed46c8691c438745c2
Swati <83855603+SwatiAF@users.noreply.github.com>
2021-12-22 12:25:04 +0530

    SEE Package created

---
+++ Packages/SEE.java
@@ -0,0 +1,25 @@
+package SEE;
+import java.SEE.*;
+import java.util.Scanner;
+class External extends Student {
+	int final_marks[] = new int[5];
+
+/*public External(int final_marks) {
+	this.final_marks = final_marks;
+}*/
+
+public void enterSEE() {
+	Scanner scan = new Scanner(System.in);
+for(int i=0; i<5; i++) {
+	System.out.println("Enter SEE marks for 5 Courses: ");
+	final_marks[i] = scan.nextInt();
+}
+}
+
+public void displaySEE() {
+for(int i=0; i<5; i++) {
+	System.out.println("SEE Marks: " + final_marks[i]);
+}
+}
+
+}	

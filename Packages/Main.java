7c247f2b2a3d3963dd799ed67210aba7a0618c9a
Swati <83855603+SwatiAF@users.noreply.github.com>
2021-12-22 12:37:07 +0530

    22/12/2021
    Main for SEE and CIE packages

---
+++ Packages/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.SEE;
+import java.CIE;
+
+public class SemDetails {
+	public static void main(String args[]) {
+	Scanner scan = new Scanner(System.in);
+	int n;
+	System.out.println("Enter the number of Students: ");
+	n = scan.nextInt();
+
+	System.out.println("Semester Details: ");
+	
+	Student ob[] = new Student(n);
+	External ob1[] = new External(n);
+	
